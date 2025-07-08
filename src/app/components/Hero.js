2f2cf6bee4a5ffe167d9a6fc5b08aca90d2b6f6a
Venkatesh T <venkatesht1243@gmail.com>
2025-07-08 12:53:28 +0530

    final

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -12,7 +12,7 @@ export default function Hero() {
         {/* Profile Picture */}
         <div className="relative w-64 h-64 md:w-80 md:h-80 rounded-full overflow-hidden border-4 border-blue-500 shadow-xl">
           <Image
-            src={imageError ? '/placeholder-profile.jpg' : '/profile.jpg'}
+            
             alt="Venkatesh T"
             fill
             className="object-cover"
